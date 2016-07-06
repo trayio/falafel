8b9896622a1271ee411faba6adbd80cfeb0d7106
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2016-07-06 15:36:14 +0100

    now handling incoming request messages elegantly

--- example-run.js
+++ example-run.js
@@ -18,13 +18,40 @@ exports.apptalk = apptalk;
 var util = require('util');
 
 // Send some exac
+// apptalk([{
+//   "id": "123-def",
+//   "header": {
+//     "message": "list_persons"
+//   },
+//   "body": {
+//     "access_token": "50349efe7fc91a5566b3f4ccc5a4c815ac98e400"
+//   }
+// }], {}, function (err, result) {
+//
+// 	console.log(util.inspect(result, false, null));
+//
+// });
+
+
+// REQUEST: Sample body, base 64 encoded
 apptalk([{
   "id": "123-def",
   "header": {
-    "message": "list_persons"
+    "message": "webhook_request"
   },
   "body": {
-    "access_token": "50349efe7fc91a5566b3f4ccc5a4c815ac98e400"
+		"input": {
+    	"access_token": "50349efe7fc91a5566b3f4ccc5a4c815ac98e400"
+		},
+		"http": {
+			"headers": {
+				// "Content-Type": ["application/json"]
+				"Content-Type": ["application/x-www-form-urlencoded"]
+			},
+			// "body": 'eyJpZCI6MTIzLCJuYW1lIjoiQ2hyaXMgSG91Z2h0b24ifQ==',
+			"body": "Zm9vW2Jhcl09YmF6",
+			"form": []
+		}
   }
 }], {}, function (err, result) {
 
