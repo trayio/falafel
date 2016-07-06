c4cf83e317aa3f049d707b31f5a100934eef1c36
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2016-07-06 16:27:46 +0100

    added tests for parsing the request body

---
+++ test/bindConnectors/bindTriggerResponse.js
@@ -0,0 +1,10 @@
+var assert      = require('assert');
+var _ 	        = require('lodash');
+var bindTrigger = require('../../lib/bindConnectors/bindTrigger');
+
+
+describe.skip('#bindTrigger', function () {
+
+
+
+});
