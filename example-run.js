ee6f0b160019944e57acf268bb1712c536c1ffac
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2016-07-06 16:14:13 +0100

    added response

--- example-run.js
+++ example-run.js
@@ -34,6 +34,34 @@ var util = require('util');
 
 
 // REQUEST: Sample body, base 64 encoded
+// apptalk([{
+//   "id": "123-def",
+//   "header": {
+//     "message": "webhook_request"
+//   },
+//   "body": {
+// 		"input": {
+//     	"access_token": "50349efe7fc91a5566b3f4ccc5a4c815ac98e400"
+// 		},
+// 		"http": {
+// 			"headers": {
+// 				"Content-Type": ["application/json"]
+// 				// "Content-Type": ["application/x-www-form-urlencoded"]
+// 			},
+// 			"body": 'eyJpZCI6MTIzLCJuYW1lIjoiQ2hyaXMgSG91Z2h0b24ifQ==',
+// 			// "body": "Zm9vW2Jhcl09YmF6",
+// 			"form": []
+// 		}
+//   }
+// }], {}, function (err, result) {
+//
+// 	console.log('error', err);
+// 	console.log(util.inspect(result, false, null));
+//
+// });
+
+
+// RESPONSE: Sample workflow data
 apptalk([{
   "id": "123-def",
   "header": {
@@ -46,16 +74,18 @@ apptalk([{
 		"http": {
 			"headers": {
 				"Content-Type": ["application/json"]
-				// "Content-Type": ["application/x-www-form-urlencoded"]
 			},
 			"body": 'eyJpZCI6MTIzLCJuYW1lIjoiQ2hyaXMgSG91Z2h0b24ifQ==',
-			// "body": "Zm9vW2Jhcl09YmF6",
 			"form": []
-		}
+		},
+		"reply": {
+      "some_data": "from the workflow",
+      "or_perhaps": "no data in case of fire_and_forget and acknowledge"
+    }
   }
 }], {}, function (err, result) {
 
-	console.log(err);
+	console.log('error', err);
 	console.log(util.inspect(result, false, null));
 
 });
