813182f15cf3d9e7e97c9c0145b11a168b4f04be
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2016-08-01 07:23:15 -0700

    fixed parse request body for dev mode on url form encoded, added tests

--- test/bindConnectors/parseRequestBody.js
+++ test/bindConnectors/parseRequestBody.js
@@ -106,6 +106,53 @@ describe('#parseRequestBody', function () {
     });
   });
 
+  it('should convert passed json object body to json in dev mode', function () {
+    var http = {
+			"headers": {
+				"Content-Type": ["application/json"]
+			},
+			"body": {
+        name: 'Chris',
+        age: 26
+      },
+			"form": []
+		};
+
+    var originalBody = _.clone(http.body);
+    var newHttp = parseRequestBody(http, {
+      dev: true
+    });
+
+    assert.deepEqual(newHttp.body, {
+      name: 'Chris',
+      age: 26
+    });
+  });
+
+  it('should not convert to base64 passed url encoded string in dev mode', function () {
+    var http = {
+			"headers": {
+				"Content-Type": ["application/x-www-form-urlencoded"]
+			},
+			"body": 'page_id=01f0d764-6639-407b-a04c-1a020e27c27e&page_name=Test&variant=a&page_url=http%3A%2F%2Funbouncepages.com%2Ftest-12344747559&data.json=%7B%22email%22%3A%5B%22ali%40tray.io%22%5D%2C%22ip_address%22%3A%5B%2277.89.130.2%22%5D%2C%22time_submitted%22%3A%5B%2210%3A28%20AM%20UTC%22%5D%7D&data.xml=%3C%3Fxml%20version%3D%271.0%27%3F%3E%0A%3Cform_data%3E%0A%20%20%3Cemail%3Eali%40tray.io%3C%2Femail%3E%0A%20%20%3Cip_address%3E77.89.130.2%3C%2Fip_address%3E%0A%20%20%3Ctime_submitted%3E10%3A28%20AM%20UTC%3C%2Ftime_submitted%3E%0A%3C%2Fform_data%3E',
+			"form": []
+		};
+
+    var originalBody = _.clone(http.body);
+    var newHttp = parseRequestBody(http, {
+      dev: true
+    });
+
+    assert.deepEqual(newHttp.body, {
+      page_id: '01f0d764-6639-407b-a04c-1a020e27c27e',
+      page_name: 'Test',
+      variant: 'a',
+      page_url: 'http://unbouncepages.com/test-12344747559',
+      'data.json': '{"email":["ali@tray.io"],"ip_address":["77.89.130.2"],"time_submitted":["10:28 AM UTC"]}',
+      'data.xml': '<?xml version=\'1.0\'?>\n<form_data>\n  <email>ali@tray.io</email>\n  <ip_address>77.89.130.2</ip_address>\n  <time_submitted>10:28 AM UTC</time_submitted>\n</form_data>'
+    });
+  });
+
 
   it('should spit out the decoded body for everything else', function () {
     var http = {
