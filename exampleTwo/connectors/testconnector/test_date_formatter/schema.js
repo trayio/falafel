59f6573bb650536e17e22e4579da92e61e9fd3fb
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2017-02-24 11:23:25 +0000

    set up a formatting value system for running every model property against the schema property

--- exampleTwo/connectors/testconnector/test_date_formatter/schema.js
+++ exampleTwo/connectors/testconnector/test_date_formatter/schema.js
@@ -4,9 +4,62 @@ module.exports = {
   input: {
 
     date: {
-      type: 'number',
+      type: 'string',
       format: 'date',
       date_mask: 'YYYY-MM-DD'
+    },
+
+    list_of_dates: {
+      type: 'array',
+      items: {
+        type: 'string',
+        format: 'date',
+        date_mask: 'X'
+      }
+    },
+
+    very_nested_list_of_dates: {
+      type: 'array',
+      items: {
+        type: 'array',
+        items: {
+          type: 'string',
+          format: 'date',
+          date_mask: 'YY-MM-DD'
+        }
+      }
+    },
+
+    my_object: {
+      type: 'object',
+      properties: {
+        date: {
+          type: 'string',
+          format: 'date',
+          date_mask: 'x'
+        },
+
+        sub_object: {
+          type: 'object',
+          properties: {
+            sub_date: {
+              type: 'number',
+              format: 'date',
+              date_mask: 'X'
+            },
+            sub_sub_object: {
+              type: 'object',
+              properties: {
+                sub_sub_date: {
+                  type: 'string',
+                  format: 'date',
+                  date_mask: 'dddd, MMMM Do YYYY, h:mm:ss a'
+                }
+              }
+            }
+          }
+        }
+      }
     }
 
   }
