8c2a0650842e265156655d3f6d9d55ede7a5caf6
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2017-03-03 11:52:07 +0000

    added the ability to handle array items

--- test/bindConnectors/formatInputBody/index.js
+++ test/bindConnectors/formatInputBody/index.js
@@ -132,4 +132,75 @@ describe('#formatInputBody', function () {
     });
   });
 
+  it('should handle items schema being an array', function () {
+    var body = {
+      my_date: '2017-10-02T00:00:00.000Z',
+      list_of_dates: [
+        '2017-10-02T00:00:00.000Z',
+        '2017-10-05T00:00:00.000Z',
+        '2017-10-01T00:00:00.000Z',
+        [
+          'chris',
+          '2016-11-02T00:00:00.000Z'
+        ],
+        '2017-11-02T00:00:00.000Z',
+        'steve'
+      ]
+    };
+    var schema = {
+      input: {
+        my_date: {
+          type: 'string',
+          format: 'date',
+          date_mask: 'YYYY-MM-DD'
+        },
+        list_of_dates: {
+          type: 'array',
+          items: [
+            {
+              type: 'string',
+              format: 'date',
+              date_mask: 'X'
+            },
+            {
+              type: 'number',
+              format: 'date',
+              date_mask: 'x'
+            },
+            {
+              type: 'string',
+              format: 'date',
+              date_mask: 'YY-MM-DD'
+            },
+            {
+              type: 'array',
+              items: [
+                {
+                  type: 'string',
+                  format: 'text'
+                }, {
+                  type: 'string',
+                  format: 'date',
+                  date_mask: 'YYYY-MM-DD'
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+
+    var result = formatInputBody(body, schema);
+
+    assert.strictEqual(body.my_date, '2017-10-02T00:00:00.000Z');
+    assert.strictEqual(result.my_date, '2017-10-02');
+    assert.strictEqual(result.list_of_dates[0], '1506902400');
+    assert.strictEqual(result.list_of_dates[1], 1507161600000);
+    assert.strictEqual(result.list_of_dates[2], '17-10-01');
+    assert.strictEqual(result.list_of_dates[3][0], 'chris');
+    assert.strictEqual(result.list_of_dates[3][1], '2016-11-02');
+    assert.strictEqual(result.list_of_dates[4], '2017-11-02T00:00:00.000Z');
+    assert.strictEqual(result.list_of_dates[5], 'steve');
+  });
+
 });
