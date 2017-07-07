3a5961ef60736bd1aa4a504367a5e8be450e89be
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2017-07-07 11:30:20 +0100

    allow test config to be passed as a string or an object

---
+++ test/parseConfig/index.js
@@ -0,0 +1,41 @@
+var _ 			= require('lodash');
+var assert 		= require('assert');
+var parseConfig = require('../../lib/parseConfig');
+var sampleConfig = require('./sampleConfig.json');
+
+
+describe('#parseConfig', function () {
+
+	it('should parse config into a falafel format', function () {
+		var parsed = parseConfig(sampleConfig);
+
+		assert(_.isArray(parsed));
+		assert(_.isObject(parsed[0]))
+		assert(_.isString(parsed[0].name))
+		assert(_.isString(parsed[0].name))
+		assert(_.isObject(parsed[0].globalModel))
+		assert(_.isObject(parsed[0].globalSchema))
+		assert(_.isArray(parsed[0].messages))
+		assert(_.isObject(parsed[0].messages[0]))
+		assert(_.isObject(parsed[0].messages[0].model))
+		assert(_.isObject(parsed[0].messages[0].schema))
+		assert(_.isString(parsed[0].messages[0].name));
+	});
+
+	it('should parse config into a falafel format, when inputted as a string', function () {
+		var parsed = parseConfig(JSON.stringify(sampleConfig));
+
+		assert(_.isArray(parsed));
+		assert(_.isObject(parsed[0]))
+		assert(_.isString(parsed[0].name))
+		assert(_.isString(parsed[0].name))
+		assert(_.isObject(parsed[0].globalModel))
+		assert(_.isObject(parsed[0].globalSchema))
+		assert(_.isArray(parsed[0].messages))
+		assert(_.isObject(parsed[0].messages[0]))
+		assert(_.isObject(parsed[0].messages[0].model))
+		assert(_.isObject(parsed[0].messages[0].schema))
+		assert(_.isString(parsed[0].messages[0].name));
+	});
+
+});
