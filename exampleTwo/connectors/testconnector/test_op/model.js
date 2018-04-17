f0fc7bd208f3949e92047bf7152bd3026b5141c8
Johnbastian <johnbastian@tray.io>
2018-04-17 19:52:53 +0100

    New threadneedle response format processing
    Tests included

--- exampleTwo/connectors/testconnector/test_op/model.js
+++ exampleTwo/connectors/testconnector/test_op/model.js
@@ -4,4 +4,13 @@ module.exports = {
 
 	url: 'https://trayio-request-bin.herokuapp.com/z9m2iqz9',
 
+	// beforeRequest: function () {
+	// 	throw new Error('Meh');
+	// },
+
+	afterHeader: function (headers, params, body, res) {
+		return { test: null };
+		// return null;
+	}
+
 };
--- test/bindConnectors/formatMessage.js
+++ test/bindConnectors/formatMessage.js
@@ -1,55 +1,90 @@
-var assert      = require('assert');
-var _ 	        = require('lodash');
+var assert = require('assert');
 var formatMessage = require('../../lib/bindConnectors/formatMessage');
 
 
 
 describe('#formatMessage', function () {
 
-  it('should format a regular message correctly, not setting version', function () {
-    var input = {
-      event: {
-        id: '123-456',
-      },
-      response: {
-        testing: true
-      },
-      version: 1
-    };
+	it('should format a regular message correctly, not setting version', function () {
+		var input = {
+			event: {
+				id: '123-456',
+			},
+			response: {
+				header: {},
+				body: {
+					testing: true
+				}
+			},
+			version: 1
+		};
 
-    var expected = {
-      id: '123-456',
-      header: {},
-      body: {
-        testing: true
-      },
-      version: undefined
-    };
+		var expected = {
+			id: '123-456',
+			header: {},
+			body: {
+				testing: true
+			},
+			version: undefined
+		};
 
-    assert.deepEqual(formatMessage(input.event, input.response, input.version), expected)
-  });
+		assert.deepEqual(formatMessage(input.event, input.response, input.version), expected);
+	});
 
-  it('should handle set version 2 when explicitly defined', function () {
-    var input = {
-      event: {
-        id: '123-456',
-      },
-      response: {
-        testing: true
-      },
-      version: 2
-    };
+	it('should format a regular message correctly with header, not setting version', function () {
+		var input = {
+			event: {
+				id: '123-456',
+			},
+			response: {
+				header: {
+					metadata: true
+				},
+				body: {
+					testing: true
+				}
+			},
+			version: 1
+		};
 
-    var expected = {
-      id: '123-456',
-      header: {},
-      body: {
-        testing: true
-      },
-      version: 2
-    };
+		var expected = {
+			id: '123-456',
+			header: {
+				metadata: true
+			},
+			body: {
+				testing: true
+			},
+			version: undefined
+		};
 
-    assert.deepEqual(formatMessage(input.event, input.response, input.version), expected)
-  });
+		assert.deepEqual(formatMessage(input.event, input.response, input.version), expected);
+	});
+
+	it('should handle set version 2 when explicitly defined', function () {
+		var input = {
+			event: {
+				id: '123-456',
+			},
+			response: {
+				header: {},
+				body: {
+					testing: true
+				}
+			},
+			version: 2
+		};
+
+		var expected = {
+			id: '123-456',
+			header: {},
+			body: {
+				testing: true
+			},
+			version: 2
+		};
+
+		assert.deepEqual(formatMessage(input.event, input.response, input.version), expected);
+	});
 
 });
