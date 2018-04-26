0b9da636e57794f431675ff693e77efb2f76544f
Johnbastian <johnbastian@tray.io>
2018-04-26 19:10:55 +0100

    Error now handling header/body format
    FormatError now handles the updated response format from threadneedle,
    i.e. header/body

--- exampleTwo/connectors/testconnector/test_op/model.js
+++ exampleTwo/connectors/testconnector/test_op/model.js
@@ -2,15 +2,16 @@ module.exports = {
 
 	method: 'get',
 
-	url: 'https://trayio-request-bin.herokuapp.com/z9m2iqz9',
+	url: '{{url}}',
 
-	// beforeRequest: function () {
-	// 	throw new Error('Meh');
-	// },
+	beforeRequest: function (request, params) {
+		if (!params.flag) {
+			throw new Error('Meh');
+		}
+	},
 
 	afterHeader: function (headers, params, body, res) {
-		return { test: null };
-		// return null;
+		return ( params.flag2 ? { test: null } : null );
 	}
 
 };
