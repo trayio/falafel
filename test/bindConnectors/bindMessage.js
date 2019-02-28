c763731db70a989b8a34e83df36d317b9b45c4cf
Johnbastian <johnbastian@tray.io>
2019-02-28 22:58:25 +0000

    Added support for afterHeaders function for function config, and tests added

--- test/bindConnectors/bindMessage.js
+++ test/bindConnectors/bindMessage.js
@@ -2,36 +2,169 @@ var assert      = require('assert');
 var _ 	        = require('lodash');
 var bindMessage = require('../../lib/bindConnectors/bindMessage');
 
+var Threadneedle = require('@trayio/threadneedle');
 
 describe('#bindMessage', function () {
 
-
-	// beforeEach(function () {
-
-	// });
-
-	it('should bind the method to threadneedle', function () {
+	it('should bind standard method to threadneedle', function () {
 		var called = false;
 		var boundMethod;
 
-		bindMessage({
-			name: 'my-message',
-			model: {
-				url: 'http://mydomain.com/link',
-				method: 'get'
+		bindMessage(
+			{
+				name: 'my-message',
+				model: {
+					url: 'http://mydomain.com/link',
+					method: 'get'
+				}
+			},
+			{
+				addMethod: function (method) {
+					called = true;
+					boundMethod = method;
+				}
+			},
+			{
+				on: function () {},
+				hasRequiredParams: function () {}
 			}
-		}, {
-			addMethod: function (method) {
-				called = true;
-				boundMethod = method;
-			}
-		}, {
-			on: function () {},
-			hasRequiredParams: function () {}
-		}, {});
+		);
 
 		assert(called);
 		assert.equal(boundMethod, 'myMessage');
+
+	});
+
+	it('should bind function method to threadneedle', function () {
+		var called = false;
+		var boundMethod;
+
+		bindMessage(
+			{
+				name: 'my-function-op',
+				model: function () {
+
+				}
+			},
+			{
+				addMethod: function (method) {
+					called = true;
+					boundMethod = method;
+				}
+			},
+			{
+				on: function () {},
+				hasRequiredParams: function () {}
+			}
+		);
+
+		assert(called);
+		assert.equal(boundMethod, 'myFunctionOp');
+
+	});
+
+	it('should execute function method when binded function is called', function (done) {
+		var called = false;
+		var boundMethod;
+
+		var threadneedle = Threadneedle();
+
+		var boundOp = bindMessage(
+			{
+				name: 'my-function-op',
+				model: function () {
+					called = true;
+					return {
+						result: true
+					};
+				},
+				schema: {}
+			},
+			threadneedle,
+			{
+				on: function () {},
+				hasRequiredParams: function () {}
+			}
+		);
+
+		boundOp({
+			body: {}
+		})
+
+		.done(
+			function (result) {
+				assert(called);
+				assert.deepEqual(
+					result.body,
+					{
+						result: true
+					}
+				);
+				done();
+			},
+			function (err) {
+				assert.fail(err);
+				done();
+			}
+		);
+
+	});
+
+	it('should execute afterHeaders function for function method', function (done) {
+		var afterHeadersCalled = false;
+		var boundMethod;
+
+		var threadneedle = Threadneedle();
+
+		var boundOp = bindMessage(
+			{
+				name: 'my-function-op',
+				model: function () {
+					return {
+						result: true
+					};
+				},
+				schema: {},
+				afterHeaders: function (error, params, body, res) {
+					afterHeadersCalled = true;
+					return params;
+				}
+			},
+			threadneedle,
+			{
+				on: function () {},
+				hasRequiredParams: function () {}
+			}
+		);
+
+		boundOp({
+			body: {
+				gotHeaderViaParams: true
+			}
+		})
+
+		.done(
+			function (result) {
+				assert(afterHeadersCalled);
+				assert.deepEqual(
+					result.headers,
+					{
+						gotHeaderViaParams: true
+					}
+				);
+				assert.deepEqual(
+					result.body,
+					{
+						result: true
+					}
+				);
+				done();
+			},
+			function (err) {
+				assert.fail(err);
+				done();
+			}
+		);
 
 	});
 
@@ -72,7 +205,7 @@ describe('#bindMessage', function () {
 			}
 		});
 
-		assert.deepEqual(requiredKeys, ['age', 'access_token']);
+		assert.deepEqual(requiredKeys, [ 'age', 'access_token' ]);
 
 	});
 
