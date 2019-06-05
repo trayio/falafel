3b2a8e8d9ff7647b7b8be40e2081dea9317e913a
Johnbastian <johnbastian@tray.io>
2019-06-05 23:57:56 +0100

    Added retrieving of code from error object or setting default. Updated default
    [CPS-89]

--- exampleTwo/connectors/testconnector/test_op4/model.js
+++ exampleTwo/connectors/testconnector/test_op4/model.js
@@ -1,7 +1,17 @@
 
 module.exports = function (params) {
-	var nonExistent = params.test.test.test;
 	return when.promise(function (resolve, reject) {
+
+		if (params.test.nonExistent) {
+			var nonExistent = params.test.test.test;
+		}
+
+		if (params.test.errorWithCode) {
+			var newErr = new Error('Test error');
+			newErr.code = 'blah';
+			throw newErr;
+		}
+
 		return resolve({
 			a: 'Hello',
 			b: 'World',
