bd22e0572b567a381c0d7798ec53fd38336896f5
Johnbastian <johnbastian@tray.io>
2019-09-10 22:35:23 +0100

    falafel now error resolves timeout like other operation errors
    Tests also updated

--- test/bindConnectors/index.js
+++ test/bindConnectors/index.js
@@ -4,7 +4,7 @@ global.when 	           = require('when');
 var bindConnectors = require('../../lib/bindConnectors');
 
 /* eslint-disable no-console */
-describe('#bindConnectors', function () {
+describe.only('#bindConnectors', function () {
 
 	global.falafel = {};
 
@@ -186,222 +186,251 @@ describe('#bindConnectors', function () {
 	});
 
 	//Test mode tests
-	var triggerConfig = [
-		{
-			name: 'test_trigger',
-			globalModel: {},
-			globalSchema: {},
-			messages: [
-				{
-					name: 'webhook',
-					schema: {
-						name: 'test_op'
-					},
-					model: function (params) {
-						return params;
-					},
-					destroy: function (params) {
-						return params;
-					},
-					request: {
-						filter: function () {
-							return true;
-						},
-						before: function (params, http) {
-							return http.body;
-						}
-					},
-					response: function (params, http, reply) {
-						return reply;
-					}
-				}
-			]
+	describe('test mode tests', function () {
 
-		}
-	];
-
-	it('should not have sub-operations in non-test mode', function () {
-
-		var boundTriggers = bindConnectors(triggerConfig, options);
-		assert.deepEqual(falafel['testTrigger']['webhookRequest'], undefined);
-		assert.ok(falafel['testTrigger']['webhookDestroy']);
-
-	});
-
-	it('should have sub-operations in test mode', function () {
-
-		var boundTriggers = bindConnectors(triggerConfig, { test: true });
-		assert.ok(falafel['testTrigger']['webhookRequest']);
-		assert.ok(falafel['testTrigger']['webhookResponse']);
-
-	});
-
-	it('should run sub-operations in test mode as normal', function (done) {
-
-		var boundTriggers = bindConnectors(triggerConfig, { test: true });
-
-		falafel['testTrigger']['webhookRequest']({
-			id: 'testID',
-			header: {
-				message: 'webhook_request'
-			},
-			body: {
-				input: {
-					hello: 'world'
-				},
-				http: {
-					method: 'GET',
-					statusCode: 200,
-					body: { success: true }
-				}
-			}
-		})
-
-		.then(function (requestRes) {
-			assert.deepEqual(JSON.parse(requestRes.body.output), { success: true });
-		})
-
-		.then(done, done);
-
-	});
-
-	//Timeout tests
-	var timeoutConnectors = bindConnectors(
-		[
+		var triggerConfig = [
 			{
-				name: 'timeout_connector',
+				name: 'test_trigger',
 				globalModel: {},
 				globalSchema: {},
 				messages: [
 					{
-						name: 'timeout_op',
+						name: 'webhook',
 						schema: {
-							name: 'timeout_op'
+							name: 'test_op'
 						},
 						model: function (params) {
 							return params;
 						},
-					},
-					{
-						name: 'timeout_op_promise',
-						schema: {
-							name: 'timeout_op_promise'
+						destroy: function (params) {
+							return params;
 						},
-						model: function (params) {
-							return when.resolve(params);
+						request: {
+							filter: function () {
+								return true;
+							},
+							before: function (params, http) {
+								return http.body;
+							}
 						},
-					},
-					{
-						name: 'long_timeout_op_promise',
-						schema: {
-							name: 'timeout_op_promise'
-						},
-						model: function (params) {
-							return when.promise(function (resolve, reject) {
-
-								setTimeout(function () {
-									resolve(params);
-								}, 21000);
-
-							});
-						},
+						response: function (params, http, reply) {
+							return reply;
+						}
 					}
 				]
 
 			}
-		],
-		options
-	);
+		];
 
-	it('should pass simple function operation run within timeout limit', function (done) {
+		it('should not have sub-operations in non-test mode', function () {
 
-		timeoutConnectors(
-			[
-				{
-					id: 'testID',
-					header: {
-						message: 'timeout_op'
-					},
-					body: {
+			var boundTriggers = bindConnectors(triggerConfig, options);
+			assert.deepEqual(falafel['testTrigger']['webhookRequest'], undefined);
+			assert.ok(falafel['testTrigger']['webhookDestroy']);
+
+		});
+
+		it('should have sub-operations in test mode', function () {
+
+			var boundTriggers = bindConnectors(triggerConfig, { test: true });
+			assert.ok(falafel['testTrigger']['webhookRequest']);
+			assert.ok(falafel['testTrigger']['webhookResponse']);
+
+		});
+
+		it('should run sub-operations in test mode as normal', function (done) {
+
+			var boundTriggers = bindConnectors(triggerConfig, { test: true });
+
+			falafel['testTrigger']['webhookRequest']({
+				id: 'testID',
+				header: {
+					message: 'webhook_request'
+				},
+				body: {
+					input: {
 						hello: 'world'
+					},
+					http: {
+						method: 'GET',
+						statusCode: 200,
+						body: { success: true }
 					}
 				}
-			],
-			{
+			})
 
-			},
-			function (err, resArr) {
-				if (err) {
-					assert.fail(err);
-				} else {
-					assert.deepEqual(resArr[0].body, { hello: 'world' });
-				}
-				done();
-			}
-		);
+			.then(function (requestRes) {
+				assert.deepEqual(JSON.parse(requestRes.body.output), { success: true });
+			})
+
+			.then(done, done);
+
+		});
 
 	});
 
-	it('should pass simple function promise operation run within timeout limit', function (done) {
 
-		timeoutConnectors(
+	//Timeout tests
+	describe('timeout tests', function () {
+
+		var timeoutConnectors = bindConnectors(
 			[
 				{
-					id: 'testID',
-					header: {
-						message: 'timeout_op_promise'
-					},
-					body: {
-						hello: 'world'
-					}
+					name: 'timeout_connector',
+					globalModel: {},
+					globalSchema: {},
+					messages: [
+						{
+							name: 'timeout_op',
+							schema: {
+								name: 'timeout_op'
+							},
+							model: function (params) {
+								return params;
+							},
+						},
+						{
+							name: 'timeout_op_promise',
+							schema: {
+								name: 'timeout_op_promise'
+							},
+							model: function (params) {
+								return when.resolve(params);
+							},
+						},
+						{
+							name: 'long_timeout_op_promise',
+							schema: {
+								name: 'timeout_op_promise'
+							},
+							model: function (params) {
+								return when.promise(function (resolve, reject) {
+
+									setTimeout(function () {
+										resolve(params);
+									}, 21000);
+
+								});
+							},
+						}
+					]
+
 				}
 			],
-			{
-
-			},
-			function (err, resArr) {
-				if (err) {
-					assert.fail(err);
-				} else {
-					assert.deepEqual(resArr[0].body, { hello: 'world' });
-				}
-				done();
-			}
+			options
 		);
 
-	});
+		it('should pass simple function operation run within timeout limit', function (done) {
 
-	it('should error on lambda function level for operation not ending within time limit', function (done) {
+			when.promise(function (resolve, reject) {
 
-		this.timeout(25000);
+				timeoutConnectors(
+					[
+						{
+							id: 'testID',
+							header: {
+								message: 'timeout_op'
+							},
+							body: {
+								hello: 'world'
+							}
+						}
+					],
+					{
 
-		timeoutConnectors(
-			[
-				{
-					id: 'testID',
-					header: {
-						message: 'long_timeout_op_promise'
 					},
-					body: {
-						hello: 'world'
+					function (opError, resArr) {
+						if (opError) {
+							assert.fail(opError);
+							reject(opError);
+						} else {
+							assert.deepEqual(resArr[0].body, { hello: 'world' });
+							resolve();
+						}
 					}
-				}
-			],
-			{
-				getRemainingTimeInMillis: function () {
-					return 20000; //20s
-				}
-			},
-			function (err, resArr) {
-				if (err) {
-					assert(err);
-				} else {
-					console.log(resArr);
-					assert.fail(resArr);
-				}
-				done();
-			}
-		);
+				);
+
+			})
+
+			.then(done, done);
+
+		});
+
+		it('should pass simple function promise operation run within timeout limit', function (done) {
+
+			when.promise(function (resolve, reject) {
+
+				timeoutConnectors(
+					[
+						{
+							id: 'testID',
+							header: {
+								message: 'timeout_op_promise'
+							},
+							body: {
+								hello: 'world'
+							}
+						}
+					],
+					{
+
+					},
+					function (opError, resArr) {
+						if (opError) {
+							assert.fail(opError);
+							reject(opError);
+						} else {
+							assert.deepEqual(resArr[0].body, { hello: 'world' });
+							resolve();
+						}
+					}
+				);
+
+			})
+
+			.then(done, done);
+
+		});
+
+		it('should error on lambda function level for operation not ending within time limit', function (done) {
+			this.timeout(10000);
+
+			when.promise(function (resolve, reject) {
+				timeoutConnectors(
+					[
+						{
+							id: 'testID',
+							header: {
+								message: 'long_timeout_op_promise'
+							},
+							body: {
+								hello: 'world'
+							}
+						}
+					],
+					{
+						getRemainingTimeInMillis: function () {
+							return 6000; //20s
+						}
+					},
+					function (timeoutError, resArr) {
+						if (timeoutError) {
+							assert.fail(timeoutError);
+							reject(timeoutError);
+						} else {
+							assert.strictEqual(resArr[0].header.error, true);
+							assert.strictEqual(resArr[0].body.code, '#connector_error');
+							assert.strictEqual(resArr[0].body.message, 'The operation timed out.');
+							assert(_.includes(resArr[0].body.payload.reason, 'The operation has timed out due to the promise not closing'));
+							assert.strictEqual(resArr[0].body.payload.operation, 'long_timeout_op_promise');
+							resolve();
+						}
+					}
+				);
+			})
+
+			.then(done, done);
+		});
 
 	});
 
