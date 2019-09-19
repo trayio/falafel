83b7f8805ab32f186227aa886955a91e5e861eb0
Johnbastian <johnbastian@tray.io>
2019-09-19 22:00:27 +0100

    Added tests to check format and updated buffer syntax

--- test/bindConnectors/bindTriggerRequest.js
+++ test/bindConnectors/bindTriggerRequest.js
@@ -1,73 +1,281 @@
-var assert		= require('assert');
-var _			= require('lodash');
-var when		= require('when');
+const assert = require('assert');
+const _ = require('lodash');
+const when = require('when');
 
-var bindTriggerRequest = require('../../lib/bindConnectors/bindTriggerRequest');
+const bindTriggerRequest = require('../../lib/bindConnectors/bindTriggerRequest');
 
-describe('#bindTrigger', function () {
+function requestResolveFormatAssert (requestData) {
+	assert(_.isPlainObject(requestData));
+
+	assert.strictEqual(requestData.version, 2);
+	assert(_.isPlainObject(requestData.headers));
+	assert(_.isPlainObject(requestData.body));
+
+	assert.deepEqual(requestData.headers, {});
+	assert.deepEqual(
+		requestData.body,
+		{
+			output: {
+				entity: 'user',
+				result: 'abc123'
+			},
+			http: {
+				headers: {},
+				body: Buffer.from('something').toString('base64')
+			}
+		}
+	);
+}
+
+function requestRejectFormatAssert (errorData) {
+	assert(_.isPlainObject(errorData));
+
+	assert(_.isPlainObject(errorData.headers));
+	assert(_.isPlainObject(errorData.body));
+
+	assert.strictEqual(errorData.body.code, '#no_trigger');
+	assert.strictEqual(errorData.body.message, 'Unverified body');
+
+	assert.deepEqual(
+		errorData.body.http,
+		{
+			status: 500,
+			body: 'Invalid request'
+		}
+	);
+}
+
+describe.only('#bindTrigger', function () {
 
 	var devOptions = {
 		dev: true
 	};
 
 	it('should return a function', function () {
-
-		var requestFunc = bindTriggerRequest(
+		const requestFunc = bindTriggerRequest(
 			{},
 			{}
 		);
-
 		assert(_.isFunction(requestFunc));
+	});
+
+	describe('should return a promise on invocation', function () {
+
+		it('request function', () => {
+
+			const requestFunc = bindTriggerRequest(
+				{
+					request: function () {}
+				},
+				devOptions
+			);
+
+			const returnedPromise = requestFunc({
+				body: {
+					input: {},
+					http: {
+						headers: {},
+						body: '{}'
+					}
+				}
+			});
+
+			assert(when.isPromiseLike(returnedPromise));
+
+		});
+
+		it('request object', () => {
+
+			const requestFunc = bindTriggerRequest(
+				{
+					request: {}
+				},
+				devOptions
+			);
+
+			const returnedPromise = requestFunc({
+				body: {
+					input: {},
+					http: {
+						headers: {},
+						body: '{}'
+					}
+				}
+			});
+
+			assert(when.isPromiseLike(returnedPromise));
+
+		});
 
 	});
 
-	it('should return a promise on invocation', function () {
+	describe('should return (resolve and reject) in correct format', function (done) {
 
-		var requestFunc = bindTriggerRequest(
-			{
-				request: function () {}
-			},
-			devOptions
-		);
+		it('function resolve', (done) => {
 
-		var returnedPromise = requestFunc({
-			body: {
-				input: {},
-				http: {
-					headers: {},
-					body: '{}'
+			const requestFunc = bindTriggerRequest(
+				{
+					request: function (params, http) {
+						return {
+							output: {
+								entity: params.entity,
+								result: JSON.parse(http.body).userId
+							},
+							http: {
+								headers: {},
+								body: Buffer.from(http.headers.challenge).toString('base64')
+							}
+						};
+					}
+				},
+				devOptions
+			);
+
+			requestFunc({
+				body: {
+					input: {
+						entity: 'user'
+					},
+					http: {
+						headers: {
+							challenge: 'something'
+						},
+						body: {
+							userId: 'abc123'
+						}
+					}
 				}
-			}
+			})
+
+			.then(requestResolveFormatAssert)
+
+			.then(done, done);
+
 		});
 
-		assert(when.isPromiseLike(returnedPromise));
+		it('function reject', (done) => {
 
+			const requestFunc = bindTriggerRequest(
+				{
+					request: function (params, http) {
+						return when.reject({
+							code: '#no_trigger',
+							message: 'Unverified body',
+							http: {
+								status: 500,
+								body: 'Invalid request'
+							}
+						});
+					}
+				},
+				devOptions
+			);
 
-		var requestFunc2 = bindTriggerRequest(
-			{
-				request: {}
-			},
-			devOptions
-		);
-
-		var returnedPromise2 = requestFunc2({
-			body: {
-				input: {},
-				http: {
-					headers: {},
-					body: '{}'
+			requestFunc({
+				body: {
+					input: {},
+					http: {
+						headers: {},
+						body: {}
+					}
 				}
-			}
+			})
+
+			.then(assert.fail)
+
+			.catch(requestRejectFormatAssert)
+
+			.then(done, done);
+
 		});
 
-		assert(when.isPromiseLike(returnedPromise2));
+		it('object resolve', (done) => {
+
+			const requestFunc = bindTriggerRequest(
+				{
+					request: {
+						before: function (params, http) {
+							return {
+								entity: params.entity,
+								result: JSON.parse(http.body).userId
+							};
+						},
+						reply: function (params, http, output) {
+							return {
+								headers: {},
+								body: http.headers.challenge
+							};
+						}
+					}
+				},
+				devOptions
+			);
+
+			requestFunc({
+				body: {
+					input: {
+						entity: 'user'
+					},
+					http: {
+						headers: {
+							challenge: 'something'
+						},
+						body: {
+							userId: 'abc123'
+						}
+					}
+				}
+			})
+
+			.then(requestResolveFormatAssert)
+
+			.then(done, done);
+
+		});
+
+		it('object reject', (done) => {
+
+			const requestFunc = bindTriggerRequest(
+				{
+					request: {
+						before: function (params, http) {
+							return when.reject({
+								code: '#no_trigger',
+								message: 'Unverified body',
+								http: {
+									status: 500,
+									body: 'Invalid request'
+								}
+							});
+						}
+					}
+				},
+				devOptions
+			);
+
+			requestFunc({
+				body: {
+					input: {},
+					http: {
+						headers: {},
+						body: {}
+					}
+				}
+			})
+
+			.then(assert.fail)
+
+			.catch(requestRejectFormatAssert)
+
+			.then(done, done);
+
+		});
 
 	});
-
 
 	it('should return defaults of filter true, before body, and reply undefined', function (done) {
 
-		var requestFunc = bindTriggerRequest(
+		const requestFunc = bindTriggerRequest(
 			{
 				request: {}
 			},
@@ -84,28 +292,24 @@ describe('#bindTrigger', function () {
 			}
 		})
 
-		.done(
-			function (response) {
+		.then(function (response) {
 
-				assert.deepEqual(
-					response,
-					{
-						version: 2,
-						headers: {},
-						body: {
-							output: '{}',
-							http: undefined
-						}
+			assert.deepEqual(
+				response,
+				{
+					version: 2,
+					headers: {},
+					body: {
+						output: '{}',
+						http: undefined
 					}
-				);
+				}
+			);
+		})
 
-				done();
-			},
-			function (err) {
-				assert.fail(err);
-				done();
-			}
-		);
+		.catch(assert.fail)
+
+		.then(done, done);
 
 	});
 
