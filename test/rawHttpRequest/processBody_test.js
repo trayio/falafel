27d52dc600d7d082af790130a17c5db806804b68
Johnbastian <johnbastian@tray.io>
2020-01-20 23:39:49 +0000

    Added processBodyServer to processBody_test.js and added/updated tests

--- test/rawHttpRequest/processBody_test.js
+++ test/rawHttpRequest/processBody_test.js
@@ -1,10 +1,26 @@
 const assert = require('assert');
+const fs = require('fs');
 
 const _ = require('lodash');
 
 const processBody = require('../../lib/rawHttpRequest/processBody.js');
 
-describe.only('processBody', () => {
+const processBodyServer = require('./processBodyServer');
+
+describe('processBody', () => {
+
+	before(() => {
+		global.falafel = {};
+		global._ = _;
+		require('../../lib/fileHandler')({
+			test: true
+		});
+		processBodyServer.start();
+	});
+	after(() => {
+		delete global.falafel;
+		processBodyServer.stop();
+	});
 
 	it('should be a function', () => {
 		assert(_.isFunction(processBody));
@@ -65,28 +81,62 @@ describe.only('processBody', () => {
 
 		});
 
-		//TODO mock file server to download file from
+		it('with files', async () => {
 
-		// it('with files', async () => {
-		//
-		// 	const sampleBody = {
-		// 		form_data: {
-		// 			id: 'abc123',
-		// 			contents: {
-		// 				url: '',
-		// 				name: '',
-		// 				mime_type: '',
-		// 				expires: ''
-		// 			}
-		// 		}
-		// 	};
-		//
-		// 	const processedBody = await processBody(sampleBody);
-		//
-		// 	assert.deepEqual(processedBody.id, sampleBody.form_data.id);
-		// 	assert.deepEqual(processedBody.contents, fileContents);
-		//
-		// });
+			const sampleBody = {
+				form_data: {
+					id: 'abc123',
+					contents: {
+						url: 'http://localhost:8787/file',
+						name: 'test.txt',
+						mime_type: 'plain/text',
+						expires: 86400
+					}
+				}
+			};
+
+			const processedBody = await processBody(sampleBody);
+
+			assert.deepEqual(processedBody.id, sampleBody.form_data.id);
+			assert.deepEqual(processedBody.contents, { file: '/tmp/test.txt', content_type: 'plain/text' });
+			assert.deepEqual(fs.readFileSync(processedBody.contents.file, 'utf8'), fs.readFileSync(`${__dirname}/processBodyServer/test.txt`, 'utf8'));
+
+		});
+
+	});
+
+	describe('should process binary', () => {
+
+		it('binary', async () => {
+
+			const sampleBody = {
+				binary: {
+					url: 'http://localhost:8787/file',
+					name: 'test.txt',
+					mime_type: 'plain/text',
+					expires: 86400
+				}
+			};
+
+			const dataStream = await processBody(sampleBody);
+
+			const processedBody = await new Promise((resolve, reject) => {
+
+				let stringAcc = '';
+
+				dataStream.on('data', (chunk) => {
+					stringAcc += chunk.toString();
+				});
+
+				dataStream.on('end', () => {
+					resolve(stringAcc);
+				});
+
+			});
+
+			assert.deepEqual(processedBody, fs.readFileSync(`${__dirname}/processBodyServer/test.txt`, 'utf8'));
+
+		});
 
 	});
 
