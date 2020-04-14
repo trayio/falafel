5d9b42ff80d239bdf1441a9cee212538f18b6f27
Johnbastian <johnbastian@tray.io>
2020-04-14 04:12:27 +0100

    example update

--- exampleThree/connectors/testconnector/file_download/model.js
+++ exampleThree/connectors/testconnector/file_download/model.js
@@ -1,14 +1,9 @@
 
 
-module.exports = function () {
+module.exports = function (params) {
 	return new Promise(function (resolve, reject) {
 
-		falafel.files.streamDownload({
-			name: 'sample.csv',
-			url: 'https://workflow-file-uploads-dev.s3.us-west-2.amazonaws.com/7474f0e7-e1f6-4b6c-bc04-69158cfa9e8b?AWSAccessKeyId=AKIAWLUGWV6AIDDMP4BN&Expires=1586851998&Signature=IqXkpeDZVRdUvf8numfu0cU9k5k%3D',
-			mime_type: 'text/csv',
-			expires: 1586851998
-		})
+		falafel.files.streamDownload(params.file)
 
 		.then((downloadObject) => {
 
