b685e5bc0cc8cbf690df8b5cd250325c24a5e451
Alistair Russell <ali@tray.io>
2022-07-14 12:04:22 +0100

    Add base url to the raw http operations input help text
    Pull the base URL from the connector global config if its there, and then inject that into the raw http operations input schema.

---
+++ test/rawHttpRequest/validateOperationConfig_test.js
@@ -0,0 +1,75 @@
+const assert = require('assert');
+
+const _ = require('lodash');
+
+const setupRawHttpRequestOperationConfig = require('../../lib/rawHttpRequest/index.js');
+
+describe('setupRawHttpRequestOperationConfig', () => {
+
+	it('should be a function', () => {
+		assert(_.isFunction(setupRawHttpRequestOperationConfig));
+	});
+
+    it('should add the base url of the connector to the endpoint input', () => {
+
+        const baseUrl = 'dummy.restapiexample.com/api/v1/employees';
+
+        const connectorsConfig = [
+                {
+                        "title": "Test Raw Op",
+                        "description": "Test",
+                        "version": "1.0",
+                        "tags": [
+                                "service"
+                        ],
+                        "icon": {
+                                "type": "streamline",
+                                "value": "globe-2"
+                        },
+                        "name": "testraw",
+                        "globalModel": {
+                                "baseUrl": baseUrl
+                        },
+                        "globalSchema": {},
+                        "messages": [
+                                {
+                                        "model": {
+                                                "method": "{{method}}",
+                                                "url": "{{url}}"
+                                        },
+                                        "schema": {
+                                                "title": "Test operation",
+                                                "globals": false,
+                                                "input": {
+                                                        "method": {
+                                                                "type": "string",
+                                                                "required": true
+                                                        },
+                                                        "url": {
+                                                                "type": "string",
+                                                                "required": true
+                                                        }
+                                                },
+                                                "name": "test_op"
+                                        },
+                                        "name": "test_op"
+                                }
+                        ],
+                        "rawHttpRequest": {
+                                "schemaOptions": {
+                                        "url": "all"
+                                }
+                        }
+                }
+        ];
+
+        const returnedConfig = setupRawHttpRequestOperationConfig(connectorsConfig);
+        const rawOperation = _.find(returnedConfig[0].messages, m => 'raw_http_request' === m.name);
+
+        assert(_.isObject(rawOperation));
+        assert(_.get(rawOperation, 'schema.input.url.oneOf[0].properties.endpoint.description') === `The endpoint to call in relation to the base URL ${baseUrl}`)
+
+
+    });
+
+});
